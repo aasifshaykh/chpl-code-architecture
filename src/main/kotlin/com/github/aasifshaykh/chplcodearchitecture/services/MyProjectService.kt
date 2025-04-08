0c3145f8958fb66772c5b6cfde2a2a2105ae9f4e
GitHub Action <action@github.com>
2025-04-08 13:09:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/aasifshaykh/chplcodearchitecture/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aasifshaykh/chplcodearchitecture/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.aasifshaykh.chplcodearchitecture.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aasifshaykh.chplcodearchitecture.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
